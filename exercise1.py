9f300964288fae5f6fa2878dc2fb0a3ab0a1377d
eden-r <e.rusnell@mail.utoronto.ca>
2015-11-02 20:42:46 -0500

    added vowels list

--- exercise1.py
+++ exercise1.py
@@ -9,10 +9,10 @@ This module converts English words to Pig Latin words
 __author__ = 'Susan Sim'
 __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
-__license__ = "MIT License"
-
-
 def pig_latinify(word):
+    output = ""
+    VOWELS = ["a, e, i, o, u"]
+
     """
     Describe your function
 
@@ -25,22 +25,25 @@ def pig_latinify(word):
     #second: search the word at index 0 to figure out if the word beings with a vowel or a consonant
         #third (if-consonant): move index 0 letter to the end
     #fourth: append either "ay" or "yay"
+    return output
 
-    word = raw_input("Please type your word: "):
 
 # set two appendix as variables
-    var1 = "yay"
-    var2 = "ay"
+var1 = "yay"
+var2 = "ay"
 
 
 # make sure the word format
-    if len(word) > 0 and word.isalpha():
-        word_lowercase = word.lower()
-        first_letter = word_lowercase [0]
+if len(word) > 0 and word.isalpha():
+    word_lowercase = word.lower()
+    first_letter = word_lowercase [0]
 
 
 # if a word begins with a vowel, append "yay" to the end of the word.
-        if first_letter = "a" or "e" or "i" or "o" or "u":
-            new_word = word_lowercase + first_letter + var1
-            new_word = new_word [1:]
-            print (new_word)
+if first_letter == "a" or "e" or "i" or "o" or "u":
+    new_word = word.lowercase() + first_letter + var1 # this doesn't make sense, as you haven't defined any of these variables
+    new_word = new_word [1:]
+    print (new_word)
+
+
+__license__ = "MIT License"
