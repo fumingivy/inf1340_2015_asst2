2877cc7dc8b37289ade0ff4d35539acf74741b61
fumingivy <mm.fu@mail.utoronto.ca>
2015-11-04 13:47:56 -0500

    exercise 1 v2

--- exercise1.py
+++ exercise1.py
@@ -19,17 +19,17 @@ def pig_latinify(word):
         first_letter = word[0]
     # check if the first letter is a vowel
         # if the first letter is a vowel, add "yay
-    # if the first letter is a consonant, more it to the end
-        # keep moving letters to the end until the first letter is a vowel
-        # add "ay" to the end of this new word
-    # if a word begins with a vowel, append "yay" to the end of the word.
     if first_letter in vowels:
         new_word = word() + first_letter + "yay" # this doesn't make sense, as you haven't defined any of these variables
         new_word = new_word[1:] # this isn't the easiest way to do this
         print (new_word)
-    for character in word:
-        
-
+       # if the first letter is a consonant, more it to the end
+        # keep moving letters to the end until the first letter is a vowel
+        # add "ay" to the end of this new word
+    # if a word begins with a vowel, append "yay" to the end of the word. for character in word:
+    for first_letter not in vowels
+        new_word = [find(vowels)] + [0:(find(vowels)-1)] +"ay"
+        print(new_word)
     """
     Describe your function
 
