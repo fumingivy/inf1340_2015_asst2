73e51447ab5c49d84724e5a702b62d8636786f8b
fumingivy <mm.fu@mail.utoronto.ca>
2015-11-04 16:10:19 -0500

    removed some redundant comments

--- exercise1.py
+++ exercise1.py
@@ -14,16 +14,12 @@ def pig_latinify(word):
     vowels = "a, e, i, o, u"
     first_letter = word[0]
     # removed lowercase method
-    # check if the first letter is a vowel
-        # if the first letter is a vowel, add "yay
+
     if first_letter in vowels:
-        new_word = word() + first_letter + "yay" # this doesn't make sense, as you haven't defined any of these variables
-        new_word = new_word[1:] # this isn't the easiest way to do this
+        new_word = word() + first_letter + "yay"
+        new_word = new_word[1:]
         print (new_word)
-       # if the first letter is a consonant, more it to the end
-        # keep moving letters to the end until the first letter is a vowel
-        # add "ay" to the end of this new word
-    # if a word begins with a vowel, append "yay" to the end of the word. for character in word:
+
     if first_letter not in vowels:
         word = word[1:] + word[0] + "ay"
 
