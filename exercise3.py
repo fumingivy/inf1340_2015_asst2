2d1ee1849cba2ba72590f48cb8c3634965508a19
eden-r <e.rusnell@mail.utoronto.ca>
2015-11-04 21:44:05 -0500

    added intersection function

--- exercise3.py
+++ exercise3.py
@@ -63,6 +63,20 @@ def intersection(table1, table2):
     row = 0
     index = 0
     new_table = []
+    in_both = False
+    for lines in table1[0:-1]:
+        row += 1
+        index = 0
+        for character in table1[row]:
+            if table1[row][index] == table2[row][index]:
+                in_both = True
+            else:
+                in_both = False
+            index += 1
+        if in_both is True:
+            new_table += table1[row]
+    return new_table
+
     # does row-by-row comparison of tables
     # does index-by-index comparison of rows
     # finds rows that are in both table1 and table2
