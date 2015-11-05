1fe9d060c1254f3bd89e5d12400961328d630bc1
eden-r <e.rusnell@mail.utoronto.ca>
2015-11-05 11:24:39 -0500

    added test cases

--- test_exercise2.py
+++ test_exercise2.py
@@ -13,6 +13,20 @@ __license__ = "MIT License"
 
 from exercise2 import find, multi_find
 
+THEME_SONG = "It's gonna get a little weird" \
+             "gonna get a little wild" \
+             "I ain't from round here" \
+             "I'm from another dimension" \
+             "gonna get a little weird" \
+             "gonna have a good! time!" \
+             "I ain't from round here" \
+             "I'm from another WHOO-HOO" \
+             "I'm talkin rainbows" \
+             "I'm talking puppies" \
+             "it's gonna get a little weird" \
+             "gonna get a little wild" \
+             "I ain't from round here" \
+             "I'm from another dimension!"
 
 def test_find_basic():
 
@@ -35,5 +49,18 @@ def test_multi_find_basic():
 # include instances where substring is not found
 # does that refer to ones where the substring isn't present in the string?
 
-def test_find_specific():
+def test_find_normal():
     assert find("I'm in class right now", "now", 0, 21) == 19
+    assert find(THEME_SONG, "dimension", 0, 337) == 92
+    assert find(THEME_SONG, "rainbows", 0, 337) ==
+    assert find(THEME_SONG, "puppies", 0, 337)
+
+def test_multi_find_normal():
+    assert multi_find(THEME_SONG, "weird", 0, 337) == "24,120,259"
+    assert multi_find(THEME_SONG, "wild", 0, 337) == "48,283"
+
+def test_not_found_find():
+    assert find("I'm busy", "Jackson", 0, 7) == "-1"
+
+def test_not_found_multi_find():
+    assert multi_find(THEME_SONG, "Star Butterfly", 0, 337) == ""
