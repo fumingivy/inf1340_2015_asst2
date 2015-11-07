5dd2c97e72814aa801317219221dbbd4a90dfcbd
eden-r <e.rusnell@mail.utoronto.ca>
2015-11-06 19:07:45 -0500

    removed "qu" and "y" excceptions

--- exercise1.py
+++ exercise1.py
@@ -11,10 +11,6 @@ __author__ = 'Eden Rusnell & Ming Fu'
 """
 assumptions:
 that the word is going to be English
-that the word is not going to include punctuation
-explanation for "qu" exception: English-speakers find it hard to understand q with a vowel other than u following it,
-we decided to have the pig_latinify function move "qu" to the end instead of just "q" (which returns the result
-"eryquay" instead of "ueryqay" for "query"). If that was undersirable, it would be simple enough to remove the exception.
 """
 
 
@@ -23,20 +19,17 @@ def pig_latinify(word):
     output = ""
     vowels = "aeiou, AEIOU"
     word = str(word)
-    if word.isalpha():
+    if word.isdigit():
+        raise ValueError
+    if word[0] in vowels:
+        return word + "yay"
+    while word[0] not in vowels:
+        word = word[1:] + word[0]
         if word[0] in vowels:
-            return word + "yay"
-        if word[0] == ("q" or "Q"):
-            if word[1] == ("u" or "U"):
-                return word[2:] + word[0:2] + "ay"
-        while word[0] not in vowels:
-            word = word[1:] + word[0]
-            if word[0] in vowels:
-                return word + "ay"
-            if word[0] is ("y" or "Y"):
-                return word + "ay"
-    else:
-        return ""
+            return word + "ay"
 
 
-#pig_latinify("word")
+
+
+
+# print pig_latinify("word")
